af5b1b42d032715a78914ac8b0d1d319ae1f5757
Anvar Khamidov <akhamidov@axevil.com>
2024-01-30 02:37:30 +0500

    feat: implement zlib:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(path.join(__dirname, "files", "archive.gz"))
+    const writeStream = fs.createWriteStream(path.join(__dirname, "files", "fileToCompress.txt"))
+    const gunzip = zlib.createGunzip()
+    readStream.pipe(gunzip).pipe(writeStream)
 };
 
 await decompress();
