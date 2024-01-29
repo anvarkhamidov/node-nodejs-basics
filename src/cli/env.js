99d17a4add01d92b72a74f7fc89eb7b97ca1cacb
Anvar Khamidov <akhamidov@axevil.com>
2024-01-29 23:47:20 +0500

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = process.env
+    let filteredVars = []
+    Object.keys(envVars).forEach(key => {
+        if (key.startsWith("RSS_")) {
+            filteredVars.push(`${key}=${envVars[key]}`)
+        }
+    })
+    console.log(filteredVars.join("; "))
 };
 
 parseEnv();
