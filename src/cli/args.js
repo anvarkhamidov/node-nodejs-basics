3f82eadeeb251403c57284c3c34f5402d314a4fd
Anvar Khamidov <akhamidov@axevil.com>
2024-01-30 00:05:20 +0500

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    let filteredArgs = []
+    args.forEach(arg => {
+        if (arg.startsWith("--")) {
+            const keyIdx = args.indexOf(arg)
+            filteredArgs.push(`${arg.slice(2)} is ${args[keyIdx + 1]}`)
+        }
+    })
+    console.log(filteredArgs.join(", "))
 };
 
 parseArgs();
