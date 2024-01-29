a4061b9ec40e9709f1aa2f042b57a6a026f95a7b
Anvar Khamidov <akhamidov@axevil.com>
2024-01-29 21:41:09 +0500

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = path.join(__dirname, "files")
+    fs.readdir(folderPath, { recursive: true }, (err, files) => {
+        if (err) {
+            throw new Error("FS operation failed")
+        }
+        files.forEach((file) => console.log(file))
+    })
 };
 
 await list();
