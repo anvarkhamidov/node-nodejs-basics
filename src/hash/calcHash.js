abad596ae72d58a9bcb3f06fb7fde10ea793eaa0
Anvar Khamidov <akhamidov@axevil.com>
2024-01-30 01:02:10 +0500

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    let hash = crypto.createHash('sha256');
+    hash.setEncoding('hex');
+    let stream = fs.createReadStream(path.join(__dirname, "files", "fileToCalculateHashFor.txt"));
+    stream.pipe(hash);
+    hash.on('data', function (data) {
+        console.log(data);
+    });
 };
 
 await calculateHash();
