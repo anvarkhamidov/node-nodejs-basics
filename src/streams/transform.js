a80aafa581aeaa4332592b3f85c666ae2e203d51
Anvar Khamidov <akhamidov@axevil.com>
2024-01-30 01:59:45 +0500

    feat: implement stream:transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from 'stream';
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, cb) {
+            const reversedChunk = chunk.toString().split('').reverse().join('');
+            cb(null, reversedChunk);
+        }
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
