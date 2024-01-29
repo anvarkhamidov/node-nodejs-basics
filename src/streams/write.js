913923e4fc52c2229c0a56acbfec3e2d61da57da
Anvar Khamidov <akhamidov@axevil.com>
2024-01-30 01:37:16 +0500

    feat: implement stream:write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    let stream = process.stdin;
+    const fp = path.join(__dirname, "files", "fileToWrite.txt");
+    const writeableStream = fs.createWriteStream(fp);
+    stream.pipe(writeableStream);
 };
 
 await write();
