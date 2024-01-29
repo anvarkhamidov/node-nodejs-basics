c244a7e14ee5d180b6a8f54ad33f2f2151a1727a
Anvar Khamidov <akhamidov@axevil.com>
2024-01-29 19:20:07 +0500

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
-const create = async () => {
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const create = async () => {
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const fp = path.join(__dirname, "files", "fresh.txt");
+    const content = 'I am fresh and young'
+
+    fs.access(fp, fs.constants.F_OK, (err) => {
+        if (err)
+            fs.writeFile(fp, content, (err) => {
+                if (err)
+                    console.log(err)
+
+            });
+        else
+            throw new Error("FS operation failed")
+    })
 };
 
 await create();
